0ef80f9b7ef978065a98545c75edd57ccbcf8e24
luks_mbp <luks@crimsoncore.be>
2020-06-11 15:04:09 +0200

    changes

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # threathunt_student
 threathunt repo for students
+
+git clone https://github.com/crimsoncore/threathunt_student.git threathunt
--- docker-compose/compose_RabbitMQ.sh
+++
@@ -1,1 +0,0 @@
-docker-compose -f docker-compose.rabbitmq.yml -f docker-compose.logstashMQ.yml -f docker-compose.portainer.yml -f docker-compose.guacamole.yml -f up -d
