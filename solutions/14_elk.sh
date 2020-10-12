8ca408c97c5ba0fa9633fcd5af7dc21e5e33b7bb
pblaton <pieterjanblaton@gmail.com>
2020-10-12 17:03:33 +0000

    elk.sh

---
+++ solutions/14_elk.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+# Install Elastic
+sudo bash -c 'echo "vm.max_map_count=262144" >> /etc/sysctl.conf'
+sudo sysctl -p
+sysctl vm.max_map_count
+
+cd /opt/threathunt/docker-compose
+cat docker-compose.elastic.yml
+
+cd /opt/threathunt/docker-compose
+sudo docker-compose -f docker-compose.elastic.yml up -d
+
+# Configure Logstash
+queue = hostname | sed  's/AZ-KALI-/RabbitQueue_Student0/g'
+
+sed -i "s/RabbitQueue_Student00/$queue/g" 100_RabbitMQ_AZURE-input.conf
+sed -i "s/PROVIDED_PASSWORD/Password1234!/g" 100_RabbitMQ_AZURE-input.conf
