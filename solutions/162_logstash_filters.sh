1ecebb7ab90b8810db33c28f76ed2dbc2d264b45
pblaton <pieterjanblaton@gmail.com>
2020-10-14 19:04:07 +0000

    logstash filters

--- solutions/162_logstash_filters.sh
+++ solutions/162_logstash_filters.sh
@@ -3,7 +3,5 @@
 # LOGSTASH configuration for FreqServer on DNS records
 cd /opt/threathunt/logstash/logstash_enrich
 cp 410_enrich_filter_windows_sysmon_dns_freq.conf /opt/threathunt/logstash/pipeline
+cp 320_enrich_sourcedest_ip.conf /opt/threathunt/logstash/pipeline
 sudo docker restart logstash_rest
-sudo docker container logs logstash_rest --follow
-
-
