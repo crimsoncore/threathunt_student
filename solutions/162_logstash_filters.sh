fda70d13c74c2c77a8bf052bf42ef20d380b598b
pblaton <pieterjanblaton@gmail.com>
2020-10-15 19:08:43 +0000

    162 logstash filters

--- solutions/162_logstash_filters.sh
+++ solutions/162_logstash_filters.sh
@@ -4,4 +4,6 @@
 cd /opt/threathunt/logstash/logstash_enrich
 cp 410_enrich_filter_windows_sysmon_dns_freq.conf /opt/threathunt/logstash/pipeline
 cp 320_enrich_sourcedest_ip.conf /opt/threathunt/logstash/pipeline
+cp 380_enrich_network.conf /opt/threathunt/logstash/pipeline
+cp 390_enrich_geo.conf /opt/threathunt/logstash/pipeline
 sudo docker restart logstash_rest
