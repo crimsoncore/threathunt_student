cdc914ce6d05a7675c52e02c6a2046c8d4e790ee
pblaton <pieterjanblaton@gmail.com>
2020-10-20 16:12:16 +0000

    remove winlogbeat ps script

--- solutions/15_winlogbeat.ps1
+++
@@ -1,2 +0,0 @@
-cd 'C:\Program Fileswinlogbeat\winlogbeat-7.8.0-windows-x86_64\'
-.\winlogbeat.exe setup --index-management -E output.logstash.enabled=false -E 'output.elasticsearch.hosts=["kali:9200"]'
